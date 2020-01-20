339903ce4417efe2f47695da290ecc1a368acb30
Clara <isleyc97@gmail.com>
2020-01-20 13:02:12 -0500

    Added test file and added function to say the remaining days

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -4,6 +4,15 @@ import "components/DayListItem.scss";
 import classnames from 'classnames';
 
 export default function DayListItem(props) {
+  function formatSpots() {
+    if (props.spots === 0) {
+      return "no spots remaining";
+    } else if (props.spots === 1) {
+      return "1 spot remaining";
+    } else {
+      return `${props.spots} spots remaining`;
+    }
+  }
   const dayClass = classnames("day-list__item", {
     "day-list__item--selected": props.selected,
     "day-list__item--full": !props.spots
@@ -11,7 +20,7 @@ export default function DayListItem(props) {
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{formatSpots()}</h3>
     </li>
   );
 }
