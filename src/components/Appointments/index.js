d1e0c953db523c5a8dd2cbbab31ea2de583c5ff7
Clara <isleyc97@gmail.com>
2020-01-21 13:53:58 -0500

    added display interviews stories

--- src/components/Appointments/index.js
+++ src/components/Appointments/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { Fragment } from 'react'
 
 import "components/Appointments/styles.scss";
-
-
+import Header from "components/Appointments/Header";
+import Show from "components/Appointments/Show";
+import Empty from "components/Appointments/Empty";
 
 export default function Appointment(props) {
 
   return (
-    <article className="appointment"></article>
+    <Fragment>
+          <article className="appointment">
+      <Header 
+        time={props.time}
+      />
+      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />}
+    </article>
+    </Fragment>
+
   )
 }
