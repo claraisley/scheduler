a045703f9ee1a8634ba014bae99d3bfa5e5edf37
Clara <isleyc97@gmail.com>
2020-01-20 19:35:26 -0500

    added error component

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,8 @@ import Empty from "components/Appointments/Empty";
 import Show from "components/Appointments/Show";
 import Confirm from "components/Appointments/Confirm";
 import Status from "components/Appointments/Status";
+import Error from "components/Appointments/Error";
+
 
 storiesOf("Button", module)
   .addParameters({
@@ -145,3 +147,4 @@ storiesOf("Button", module)
         .add("Show", () => <Show student="Lydia Miller-Jones" interviewer={interviewers[0]} onEdit={action("onEdit")} onDelete={action("onDelete")} />)
         .add("Confirm", () => <Confirm message="Delete Appointment?" onConfirm={action("onConfirm")} onCancel={action("onCancel")} />)
         .add("Status", () => <Status message="Deleting" />)
+        .add("Error", () => <Error message="Could not delete appointment" onClose={action("onClose")} />)
