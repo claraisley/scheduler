392aaed957d97a5aa91aa8cdfb303b300d3cfcd3
Clara <isleyc97@gmail.com>
2020-01-22 17:03:43 -0500

    Implemented custom hook for 3 views

--- src/components/Appointments/index.js
+++ src/components/Appointments/index.js
@@ -4,8 +4,19 @@ import "components/Appointments/styles.scss";
 import Header from "components/Appointments/Header";
 import Show from "components/Appointments/Show";
 import Empty from "components/Appointments/Empty";
+import Form from "components/Appointments/Form";
+import useVisualMode from "hooks/useVisualMode";
 
 export default function Appointment(props) {
+
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+  const interviewers = [];
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
 
   return (
     <Fragment>
@@ -13,7 +24,19 @@ export default function Appointment(props) {
       <Header 
         time={props.time}
       />
-      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+      <Show
+        student={props.interview.student}
+        interviewer={props.interview.interviewer}
+      />
+    )}
+    {mode === CREATE && (
+    <Form interviewers={interviewers} 
+    onSave={props.onSave} 
+    onCancel={() => back(EMPTY)}
+    />
+    )}
     </article>
     </Fragment>
 
