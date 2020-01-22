1dd61ed582d39a2372bc755407dfc4257e3fa136
Clara <isleyc97@gmail.com>
2020-01-21 19:06:52 -0500

    added api call to get days, added helpers folder and test file

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,5 @@
-import React, { useState, Fragment } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import "components/Application.scss";
 import DayList from "components/DayList";
@@ -6,23 +7,6 @@ import Appointment from "components/Appointments";
 
 
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = [
   {
@@ -73,6 +57,15 @@ const appointments = [
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios.get(
+      '/api/days'
+    )
+      .then(resp => setDays(resp.data))
+  
+    }, [])
   
   const appts = appointments.map(appointment => {
     return (
