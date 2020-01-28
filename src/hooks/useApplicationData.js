95396ce75aef04f6307523a191e486239a84cd32
Clara <isleyc97@gmail.com>
2020-01-28 12:51:59 -0500

    added cypress, refactored reducer

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,34 +1,10 @@
 import { useEffect, useReducer } from 'react';
 import axios from "axios";
-
-
-const SET_DAY = "SET_DAY";
-const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
-const SET_INTERVIEW = "SET_INTERVIEW";
-
-function reducer(state, action) {
-  switch (action.type) {
-    case SET_DAY:
-      return { ...state, day: action.day }
-    case SET_APPLICATION_DATA:
-      return { ...state, days: action.days, appointments: action.appointments, interviewers: action.interviewers }
-    case SET_INTERVIEW: {
-      const { id, interview } = action;
-      return    { ...state,
-      appointments: {
-        ...state.appointments,
-        [id]: {
-          ...state.appointments[action.id],
-          interview:action.interview ? { ...interview } : null
-        }
-      }}
-    }
-    default:
-      throw new Error(
-        `Tried to reduce with unsupported action type: ${action.type}`
-      );
-  }
-}
+import reducer, {
+  SET_DAY,
+  SET_APPLICATION_DATA,
+  SET_INTERVIEW
+} from "reducers/application";
 
 
 export default function useApplicationData() {
